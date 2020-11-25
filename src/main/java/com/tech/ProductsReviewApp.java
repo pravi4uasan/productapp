474929625152dfe46f750035dabd8cba836a03d7
pravi4uasan <39478176+pravi4uasan@users.noreply.github.com>
2020-11-25 21:59:23 +0530

    Discovery service changes
    Discovery service changes

--- src/main/java/com/tech/ProductsReviewApp.java
+++ src/main/java/com/tech/ProductsReviewApp.java
@@ -2,10 +2,12 @@ package com.tech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductsReviewApp {
 
 	public static void main(String[] args) {
