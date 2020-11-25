3ad23f7831573d773e0e6dae8135c208bd37873c
pravi4uasan <39478176+pravi4uasan@users.noreply.github.com>
2020-11-25 20:40:14 +0530

    K8s config file changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,8 +8,16 @@ CREATE TABLE product_reviews (
 );
  
 INSERT INTO product_reviews (product_id, review_score, review_comment) VALUES
-  ('BB5476', 4, 'good one'),
+  ('BB5476', 5, 'Excellent'),
+  ('BB5476', 4, 'Good'),
+  ('BB5476', 3, 'Average'),
+  ('BB5476', 2, 'Ok Ok'),
+  ('BB5476', 1, 'Bad'),
+  ('M20324', 5, 'Excellent'),
+  ('M20324', 4, 'Good'),
   ('M20324', 3, 'avg quality'),
+  ('M20324', 2, 'To be ok'),
+  ('M20324', 1, 'Very Bad'),
   ('ABC123', 2, 'need ok'),
   ('BB5476', 4, 'good one'),
   ('M20324', 3, 'avg quality'),
